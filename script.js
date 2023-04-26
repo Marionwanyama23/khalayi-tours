8862266e41b4f0459a55ba7d43c97179958adb86
+ <+>
2023-04-26 19:16:32 +0300

    commit

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ window.onscroll = () =>{
 }
 
 let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+};
