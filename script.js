a8a32ecbeceefada6d3f5b7ce38cbf60440ee22a
+ <+>
2023-04-26 19:17:44 +0300

    location

--- script.js
+++ script.js
@@ -53,3 +53,12 @@ var swiper = new Swiper(".review-slider", {
         el: ".swiper-pagination",
     },
 });
+
+if ('geolocation' in navigator){
+    console.log ('geolocation available');
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+    });
+}else {
+    console.log ('geolocation not available');
+}
