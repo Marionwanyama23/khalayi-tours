0da0ea27e4861ce22ea4185b01064198614624ff
+ <+>
2023-04-26 19:17:11 +0300

    commit

--- script.js
+++ script.js
@@ -40,3 +40,16 @@ themeBtn.onclick = () =>{
     }
 
 };
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
